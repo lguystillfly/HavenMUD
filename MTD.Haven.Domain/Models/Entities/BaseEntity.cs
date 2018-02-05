774cf93cb773425fb173b90e673d4ded37db716c
Matt Smith <msmith@valorem.com>
2018-02-05 12:34:55 -0600

    Added health / mana / movement to base entity.

--- MTD.Haven.Domain/Models/Entities/BaseEntity.cs
+++ MTD.Haven.Domain/Models/Entities/BaseEntity.cs
@@ -7,6 +7,9 @@ namespace MTD.Haven.Domain.Models.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
+        public int Health { get; set; }
+        public int Mana { get; set; }
+        public int Movement { get; set; }
         public int CurrentRoom { get; set; }
 
         void Move(int id)
